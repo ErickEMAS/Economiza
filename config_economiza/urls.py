cb433ec527e616ab0f9ca4dcafeb659d0a3217d6
ErickEMAS <erick8alexandre@gmail.com>
2019-11-30 22:46:26 -0300

    Direcionar url do home.html

--- config_economiza/urls.py
+++ config_economiza/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from economiza import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mostrar_home),
 ]
