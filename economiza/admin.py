bc7a7ba206bd0b94d4d1baba19d360b8b5bf6632
Lucas <lucasrufinomaiellaro@gmail.com>
2019-12-04 14:01:36 -0300

    registrar Comercios no admin

--- economiza/admin.py
+++ economiza/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from economiza.models import Produto
+from economiza.models import Produto, Comercio
 
 # Register your models here.
 
 admin.site.register(Produto)
+admin.site.register(Comercio)
