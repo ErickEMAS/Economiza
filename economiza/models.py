a646a0d8d4074a0be84adebf4bdfd1f4da8b76ba
ErickEMAS <erick8alexandre@gmail.com>
2019-12-07 23:09:22 -0300

    Adicionar novas caracteristicas aos itens

--- economiza/models.py
+++ economiza/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=50)
-    preco = models.DecimalField(decimal_places=2, max_digits=1000, default=50)
-
-    def __str__(self):
-        return self.nome
-
 class Comercio(models.Model):
     nome = models.CharField(max_length=100)
     rua = models.CharField(max_length=200)
     bairro = models.CharField(max_length=200)
-    numero = models.PositiveIntegerField(default=0)
+    cidade = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nome + ' ' + self.bairro + ' ' + self.cidade
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE)
+    preco = models.DecimalField(decimal_places=2, max_digits=1000, default=50)
 
     def __str__(self):
         return self.nome
