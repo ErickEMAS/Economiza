bdfbe94ac963092d2cd6785e492ce90248e926de
izameelo <izabelmelo3@gmail.com>
2019-12-09 20:18:07 -0300

    Corrigir erro no menu mobile

--- economiza/static/JS/scripts.js
+++ economiza/static/JS/scripts.js
@@ -1,11 +1,7 @@
 function abrirMenu (){
     let menu = document.querySelector('.open-nav');
     menu.classList.toggle('close');
-    console.log ('Menuteste')
-}
-
-function mostrarMenu (){
-    let menu = document.querySelector('.itens-nav');
-    menu.classList.toggle('active');
+    let menuItens = document.querySelector('.menu');
+    menuItens.classList.toggle('active');
     console.log ('Menuteste')
 }
