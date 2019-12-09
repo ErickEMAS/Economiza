f16ae3bee2c765d207d53aa7aa8acc94f07ed95c
izameelo <izabelmelo3@gmail.com>
2019-12-09 19:54:51 -0300

    Criar animação do Menu Mobile

---
+++ economiza/static/JS/scripts.js
@@ -0,0 +1,11 @@
+function abrirMenu (){
+    let menu = document.querySelector('.open-nav');
+    menu.classList.toggle('close');
+    console.log ('Menuteste')
+}
+
+function mostrarMenu (){
+    let menu = document.querySelector('.itens-nav');
+    menu.classList.toggle('active');
+    console.log ('Menuteste')
+}
