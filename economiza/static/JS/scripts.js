2734b6a64978fd876ef3f7a5a5166b1846929e07
ErickEMAS <erick8alexandre@gmail.com>
2019-12-10 20:33:45 -0300

    Deletar pasta com nome incorreto

--- economiza/static/JS/scripts.js
+++
@@ -1,7 +0,0 @@
-function abrirMenu (){
-    let menu = document.querySelector('.open-nav');
-    menu.classList.toggle('close');
-    let menuItens = document.querySelector('.menu');
-    menuItens.classList.toggle('active');
-    console.log ('Menuteste')
-}
