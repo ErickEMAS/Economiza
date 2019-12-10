c57c8d3a64ba9d4e8be4561ec86de7cba22849b8
ErickEMAS <erick8alexandre@gmail.com>
2019-12-10 20:32:19 -0300

    Direcionar scripts.js ao diretorio correto

---
+++ economiza/static/js/scripts.js
@@ -0,0 +1,7 @@
+function abrirMenu(){
+    let menu = document.querySelector('.open-nav');
+    menu.classList.toggle('close');
+    let menuItens = document.querySelector('.menu');
+    menuItens.classList.toggle('active');
+    console.log ('Menuteste')
+}
