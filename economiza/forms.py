df8d742d065bbb6876f034cb7efa194d4a2e0725
unknown <lucasrufinomaiellaro@gmail.com>
2019-12-09 21:24:56 -0300

    Criar formulário UsuarioForm

--- economiza/forms.py
+++ economiza/forms.py
@@ -1,48 +1,8 @@
 # _*_ conding: utf-8 _*_
 from django import forms
 from django.forms import ModelForm
-from economiza.models import Produto, Comercio
+from economiza.models import Produto, Comercio, Usuario
 from django.contrib.auth.models import User
-
-class UserModelForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = [
-                    'username',
-                    'first_name',
-                    'last_name',
-                    'email',
-                    'password',
-        ]
-        widgets = {
-                    'username': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
-                    'first_name': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
-                    'last_name': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
-                    'email': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
-                    'password': forms.PasswordInput(attrs={'class': 'form-control', 'maxlength': 255}),                    
-        }
-
-        error_messages = {
-            'username': {
-                'required': 'Este campo é obrigatório'
-            },
-
-            'first_name': {
-                'required': 'Este campo é obrigatório'
-            },
-
-            'last_name': {
-                'required': 'Este campo é obrigatório'
-            },
-
-            'email': {
-                'required': 'Este campo é obrigatório'
-            },
-
-            'password': {
-                'required': 'Este campo é obrigatório'
-            },
-        }
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
@@ -65,3 +25,14 @@ class ComercioForm(forms.ModelForm):
                     'numero'
         ]
 
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = [
+                    'nome',
+                    'senha',
+                    'email'
+        ]
+        widgets = {
+                    'senha': forms.PasswordInput(),
+        }
